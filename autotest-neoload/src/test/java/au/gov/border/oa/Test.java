b703b8d8fa55a223726651ce75c5bb929d98b5a0
Stephane Martin <stephane.martin@neotys.com>
2019-06-10 16:25:06 +0200

    Add assembly to embed the dependencies

--- autotest-neoload/src/test/java/au/gov/border/oa/Test.java
+++ autotest-neoload/src/test/java/au/gov/border/oa/Test.java
@@ -1,6 +1,6 @@
 package au.gov.border.oa;
 import au.gov.border.oa.runjunit.features.student.*;
-public class test {
+public class Test {
     public static void main(String[] args) throws Exception{
 
     new Student().viewStudyVisaDetails();
